f2a30bd64fa6975e64e44f8b1dd62d0ab06d8274
Fikret Aslan <f.aslan0191@gmail.com>
2023-11-13 09:44:16 +0000

    server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// add your endpoints here
+
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
