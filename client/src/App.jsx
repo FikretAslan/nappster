e7bd60cba42c49024aed2c955697a0a54e51ce4f
Dom <dom.benatar@hotmail.com>
2023-11-14 16:00:06 +0000

    server results

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,53 +4,61 @@ import Feeding from "./components/Feeding";
 import Nappies from "./components/Nappies";
 import Sleep from "./components/Sleep";
 import Home from "./components/Home";
+import NewFeed from "./components/NewFeed";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [feeds, setFeeds] = useState([]);
+  useEffect(() => {
+    getFeed();
+  }, []);
 
   async function getFeed() {
     const API = "http://localhost:8080/feeding";
     const axiosRes = await axios.get(API);
     setFeeds(axiosRes.data);
-
-    return (
-      <BrowserRouter>
-        <header>
-          <img src="https://placehold.co/200x100" alt="Logo" />
-
-          <h1>
-            <Link to="/">Nappster</Link>
-          </h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/feeding">Feeding</Link>
-              </li>
-              <li>
-                <Link to="/nappies">Nappies</Link>
-              </li>
-              <li>
-                <Link to="/sleep">Sleep</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route path="/feeding" element={<Feeding />} />
-
-          <Route path="/nappies" element={<Nappies />} />
-
-          <Route path="/sleep" element={<Sleep />} />
-        </Routes>
-        <footer>
-          &copy;Nappster - Making parents' lives easier since 2023
-        </footer>
-      </BrowserRouter>
-    );
   }
+
+  return (
+    <BrowserRouter>
+      <header>
+        <img src="https://placehold.co/200x100" alt="Logo" />
+
+        <h1>
+          <Link to="/">Nappster</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/feeding">Feeding</Link>
+            </li>
+            <li>
+              <Link to="/nappies">Nappies</Link>
+            </li>
+            <li>
+              <Link to="/sleep">Sleep</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/feeding"
+          element={<Feeding feeds={feeds} setFeeds={setFeeds} />}
+        />
+
+        <Route path="/nappies" element={<Nappies />} />
+
+        <Route path="/sleep" element={<Sleep />} />
+      </Routes>
+
+      <footer>&copy;Nappster - Making parents' lives easier since 2023</footer>
+    </BrowserRouter>
+  );
 }
+
 export default App;
