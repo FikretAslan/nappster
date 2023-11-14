117afe4518ae20e25ebc6a65a2b3506a338c1886
Dom <dom.benatar@hotmail.com>
2023-11-14 14:38:41 +0000

    form

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
